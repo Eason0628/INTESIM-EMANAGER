f497973b42cde33f0e9f31fd86fd1e1a399984b5
Eason0628 <zhaowintoo@gmail.com>
2025-11-04 17:51:37 +0800

    9.Add Axios and NProgress to the project: run the commands npm install axios and npm install nprogress, then create http.js.

---
+++ packages/project/emanager/src/js/constant.js
@@ -0,0 +1,4 @@
+export const DASHBOARD_URL = "/layout/dashboard";
+export const TOKEN_KEY = "intesim_token";
+export const THING_CODE_KEY = "intesim_thing_code";
+export const THING_CODE = "p601_md"; 
--- packages/project/emanager/src/store/module/user.js
+++ packages/project/emanager/src/store/module/user.js
@@ -1,11 +1,24 @@
+import { TOKEN_KEY } from "@/js/constant";
 export default {
     namespaced: true,
     state: {
+        token: localStorage.getItem(TOKEN_KEY) ? localStorage.getItem(TOKEN_KEY) : "",
+        userInfo: {},
     },
     mutations: {
-
+        setToken(state, token) {
+            state.token = token;
+            if (token) localStorage.setItem(TOKEN_KEY, token);
+            else {
+                localStorage.removeItem(TOKEN_KEY);
+                location.href = "/login";
+            }
+        },
     }, actions: {
 
     }, getters: {
+        token(state) {
+            return state.token;
+        },
     }
 }
